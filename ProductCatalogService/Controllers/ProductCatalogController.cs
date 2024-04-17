48a00a199f9f815e10fcf7a5c4209c0e0c9060fc
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-04-17 01:43:41 -0500

    Creación  de servicios para exponer el servicio

--- ProductCatalogService/Controllers/ProductCatalogController.cs
+++ ProductCatalogService/Controllers/ProductCatalogController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using ProductCatalogService.Models;
+using ProductCatalogService.Services;
 
 namespace ProductCatalogService.Controllers
 {
@@ -8,21 +8,23 @@ namespace ProductCatalogService.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly List<Product> _products = new List<Product>
+        private readonly ProductsService _productCatalogService;
+
+        public ProductsController(ProductsService productCatalogService)
         {
-            new Product { Id = 1, Name = "Product 1", Price = 10.99 , Amount = 20},
-            new Product { Id = 2, Name = "Product 2", Price = 20.99 , Amount = 20 },
-        };
+            _productCatalogService = productCatalogService;
+        }
 
         [HttpGet]
         public IEnumerable<Product> Get()
         {
-            return _products;
+            return _productCatalogService.GetAllProducts();
         }
+
         [HttpGet("{id}")]
         public ActionResult<Product> Get(int id)
         {
-            var product = _products.Find(p => p.Id == id);
+            var product = _productCatalogService.GetProductById(id);
             if (product == null)
             {
                 return NotFound();
@@ -33,9 +35,8 @@ namespace ProductCatalogService.Controllers
         [HttpPost]
         public ActionResult<Product> Post(Product product)
         {
-            _products.Add(product);
+            _productCatalogService.AddProduct(product);
             return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
         }
     }
-
 }
