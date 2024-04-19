972bf9c86b76add25df3a2261b976483a1677073
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-04-18 21:57:38 -0500

    Archivos generados bin

--- ProductCatalogService/obj/Debug/net8.0/ProductCatalogService.AssemblyInfo.cs
+++ ProductCatalogService/obj/Debug/net8.0/ProductCatalogService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductCatalogService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+221319a8073d81b7242940544108aca98da12678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb7d2d9aace68c981ff73e1342f643ee3eeef55")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductCatalogService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductCatalogService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
