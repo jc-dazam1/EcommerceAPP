0e7ebacefaae06f76338566cf99ed2764551e97b
Juan Daza <jc.dazam1@uniandes.edu.co>
2024-04-22 17:43:05 -0500

    Generacion archivo dependences.json

--- OrderManagmentFunction/dynamodb.js
+++ OrderManagmentFunction/dynamodb.js
@@ -1,16 +1,10 @@
-// Configuración del cliente DynamoDB para LocalStack
 const { DynamoDB } = require('@aws-sdk/client-dynamodb');
-
-// Configuración del cliente DynamoDB para LocalStack
-const dynamoDB = new DynamoDB({ 
-    region: 'us-east-1', // Región
-    endpoint: 'http://localstack:4566' // URL de LocalStack
-});
-
+const dynamoDB = new DynamoDB({ region: process.env.AWS_DEFAULT_REGION, endpoint: process.env.AWS_ENDPOINT_URL });
 
 async function createOrderTable() {
+    const tableName = 'Orders';
     const params = {
-        TableName: 'Orders',
+        TableName: tableName,
         KeySchema: [
             { AttributeName: 'productId', KeyType: 'HASH' },
         ],
@@ -24,13 +18,30 @@ async function createOrderTable() {
     };
 
     try {
-        await dynamoDB.createTable(params);
-        console.log("Orders table created successfully");
+        // Verificar si la tabla ya existe
+        await dynamoDB.describeTable({ TableName: tableName });
+
+        console.log("La tabla", tableName, "ya existe.");
     } catch (error) {
-        console.error("Error creating Orders table:", error);
+        // Si la tabla no existe, crearla
+        if (error.name === 'ResourceNotFoundException') {
+            console.log("La tabla", tableName, "no existe. Creándola...");
+            try {
+                await dynamoDB.createTable(params);
+                console.log("Tabla", tableName, "creada exitosamente.");
+            } catch (error) {
+                console.error("Error creating Orders table:", error);
+                throw error;
+            }
+        } else {
+            // Si ocurre otro tipo de error, lanzar la excepción
+            console.error("Error describing Orders table:", error);
+            throw error;
+        }
     }
 }
 
 module.exports = {
     createOrderTable,
 };
+
--- OrderManagmentFunction/orderHandler.js
+++ OrderManagmentFunction/orderHandler.js
@@ -1,35 +1,65 @@
-const {
-    DynamoDBDocument,
-} = require('@aws-sdk/lib-dynamodb');
 
-const {
-    DynamoDB,
-} = require('@aws-sdk/client-dynamodb');
+const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { ScanCommand } = require('@aws-sdk/client-dynamodb');
 
 const http = require('http');
 
 const dynamoDB = new DynamoDB({ region: process.env.AWS_DEFAULT_REGION, endpoint: process.env.AWS_ENDPOINT_URL });
+const dynamoDBClient = new DynamoDBClient({ 
+    region: process.env.AWS_DEFAULT_REGION,
+    endpoint: process.env.AWS_ENDPOINT_URL
+});
 
 exports.handler = async (event) => {
+    if (event.httpMethod === 'POST' && event.path === '/order') {
+        return await placeOrder(event);
+    } else if (event.httpMethod === 'GET' && event.path === '/orders') {
+        return await getOrders();
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Invalid request' }),
+        };
+    }
+};
+
+async function placeOrder(event) {
     // Obtener los datos del producto del evento
     const { productId, quantity } = JSON.parse(event.body);
-
     // Obtener información del producto del catálogo (simulado aquí)
     const productInfo = getProductInfo(productId);
-
     // Calcular el precio total de la orden
     const totalPrice = productInfo.price * quantity;
-
     // Guardar la orden en DynamoDB
     await saveOrderToDynamoDB(productId, quantity, totalPrice);
-
     // Retornar una respuesta exitosa
     return {
         statusCode: 200,
         body: JSON.stringify({ message: 'Order placed successfully!' }),
     };
-};
+}
 
+async function getOrders() {
+    const params = {
+        TableName: 'Orders',
+    };
+
+    try {
+        const command = new ScanCommand(params);
+        const data = await dynamoDBClient.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Items),
+        };
+    } catch (error) {
+        console.error("Error fetching orders:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error fetching orders' }),
+        };
+    }
+}
 function getProductInfo(productId) {
     // Aquí puedes simular la obtención de información del producto del catálogo (p. ej., llamada a otro servicio)
     return {
@@ -59,7 +89,7 @@ async function saveOrderToDynamoDB(productId, quantity, totalPrice) {
     }
 }
 
-
+/*
 const server = http.createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/order') {
         let body = '';
@@ -79,3 +109,4 @@ const server = http.createServer((req, res) => {
 server.listen(3001, () => {
     console.log('Server listening on port 3001');
 });
+*/
